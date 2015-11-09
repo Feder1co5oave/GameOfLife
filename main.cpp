331c494d8bed29277befb3f8f207dd2db1066ffa
lucarin91 <lucarin91@gmail.com>
2015-11-09 18:13:59 +0100

    implement also sequential version

--- main.cpp
+++ main.cpp
@@ -1,35 +1,100 @@
 #include <iostream>
+#include <algorithm>
 #include <functional>
 #include <chrono>
 #include <thread>
+#include <vector>
 #include "Matrix.hpp"
+#include "MatrixG.hpp"
+
+char* getArgument(int argc, char **argv, const std::string& option) {
+  char **end  = argv + argc;
+  char **itOp = std::find(argv, end, option);
+
+  if ((itOp != end) && (++itOp != end)) return *itOp;
+  else return 0;
+}
+
+bool existArgument(int argc, char **argv, const std::string& option) {
+  char **end = argv + argc;
+
+  return std::find(argv, end, option) != end;
+}
+
+short lifeLogic (int i, int j, short v, int alive){
+  if (v == 1) {
+    if (alive < 2) return 0;
+
+    if (alive > 3) return 0;
+  } else if (v == 0) {
+    if (alive == 3) return 1;
+  }
+  return v;
+}
+
+void bodyThread (Matrix *m, int start, int end) {
+  m->forEach(start,end,lifeLogic);
+}
+
+void bodySequential (Matrix *m) {
+  m->forEach(lifeLogic);
+}
 
 int main(int argc, char *argv[]) {
-  // number of threads to be used
-  // int nw = std::atoi(argv[1]);
-  int h     = 800;
-  int w     = 600;
-  Matrix *m = new Matrix(h, w);
-  m->printG();
-
-
-  // m->set(-23,40,100);
-  std::cout << m->get(-1,0)<<std::endl;
-  m->printG();
-  for (int i = 0; i < 1000; i++) {
-    m->forEach([&](int i, int j, short *v, int alive, int death) {
-      // std::cout << i << j << *v << alive << std::endl;
-      if (*v == 1) {
-        if (alive < 2) *v = 0;
-
-        if (alive > 3) *v = 0;
-      }else if (*v == 0) {
-        if (alive == 3) *v = 1;
-      }
-    });
-    m->printG();
-    // std::cout << "iteration " << i << std::endl;
+  if (existArgument(argc, argv, "--help") ||
+      existArgument(argc, argv, "-h")) {
+    std::cout << "--thread <number> \t number of threads" << std::endl;
+    std::cout << "--height <number> \t height of the matrix" << std::endl;
+    std::cout << "--width <number> \t width of the matrix" << std::endl;
+    std::cout << "--step <number> \t number of step" << std::endl;
+    std::cout << "--graphic \t\t activate the graphic mode" << std::endl;
+    std::cout << "--help or -h \t\t this help" << std::endl;
+    return 0;
   }
 
+  char *nwStr = getArgument(argc, argv, "--thread");
+  int   nw    = nwStr ? std::atoi(nwStr) : std::thread::hardware_concurrency();
+
+  char *hStr = getArgument(argc, argv, "--height");
+  int   h    = hStr ? std::atoi(hStr) : 100;
+
+  char *wStr = getArgument(argc, argv, "--width");
+  int   w    = wStr ? std::atoi(wStr) : 100;
+
+  char *sStr = getArgument(argc, argv, "--step");
+  int   s    = sStr ? std::atoi(sStr) : 100;
+
+  Matrix *m = nullptr;
+  if (existArgument(argc, argv, "--graphic"))
+     m = new MatrixG(h, w);
+  else
+     m = new Matrix(h, w);
+
+  for (int k = 0; k < s || s==0; k++) {
+    std::cout << "step " << k << std::endl;
+    if (nw!=0){
+      int nRow = (h / nw);
+      std::vector<std::thread> tid;
+
+
+      for (int i = 0; i < nw; i++) {
+        int start = nRow * i;
+        int end   = i != nw - 1 ? start + nRow : h;
+        std::cout << "thread" << i << " start: " << start << " end:" << end <<
+          std::endl;
+
+        tid.push_back(std::thread(bodyThread, m, start, end));
+      }
+
+      for (int i = 0; i < nw; i++) tid[i].join();
+    }else{
+      bodySequential(m);
+    }
+
+    m->swap();
+    m->print();
+
+  }
+  delete m;
   return 0;
 }
--- makefile
+++ makefile
@@ -1,19 +1,27 @@
-all_build: build s.out t.out test.out
+all_build: build main.out
 
-s.out: sequential.cpp build/Matrix.o
-	g++ -g -std=c++11 sequential.cpp build/Matrix.o -o s.out
-
-t.out: thread.cpp build/Matrix.o build/MatrixG.o
-	g++ -g thread.cpp build/MatrixG.o build/Matrix.o -std=c++11 -pthread `pkg-config --cflags --libs opencv` -o t.out
-
-test.out: test.cpp build/MatrixG.o build/Matrix.o
-	g++ -g test.cpp build/MatrixG.o build/Matrix.o -std=c++11 -pthread `pkg-config --cflags --libs opencv` -o test.out
+main.out: main.cpp build/Matrix.o build/MatrixG.o
+	g++ main.cpp build/MatrixG.o build/Matrix.o \
+			-std=c++11 \
+			-pthread \
+			`pkg-config --cflags --libs opencv` \
+			-g \
+			-o main.out
 
 build/MatrixG.o: MatrixG.cpp
-	g++ -g -c -std=c++11 `pkg-config --cflags --libs opencv` MatrixG.cpp -o build/MatrixG.o
+	g++ MatrixG.cpp \
+			-g \
+			-c \
+			-std=c++11 \
+			`pkg-config --cflags --libs opencv` \
+			-o build/MatrixG.o
 
 build/Matrix.o: Matrix.cpp
-		g++ -g -c -std=c++11 Matrix.cpp -o build/Matrix.o
+		g++ Matrix.cpp \
+				-g \
+				-c \
+				-std=c++11 \
+				-o build/Matrix.o
 
 build:
 	mkdir -p build
