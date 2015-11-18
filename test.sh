5e6b560ad6ad4d7b432a576affec6ed82e4d9ea5
lucarin91 <lucarin91@gmail.com>
2015-11-18 22:46:10 +0100

    update makefile

--- test.sh
+++ test.sh
@@ -1,1 +1,8 @@
-time ./main.xeon.out --step 1000 --height 1000 --width 1000 --thread 16 
+#!/bin/bash
+
+for i in {1..20}
+do
+  time ./main.xeon.out --step 1000 --height 1000 --width 1000 --thread 16
+  echo "\n"
+  sleep 1
+done
