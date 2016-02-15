e63f7fe857081fc41e60ad8ce942410c5df3cb4e
Federico Soave <fede.covr@gmail.com>
2016-02-15 19:39:54 +0100

    test scripts now generate actual json

--- test_mic1.pl
+++ test_mic1.pl
@@ -1,24 +1,27 @@
 #!/bin/perl -w
-use constant N => 8;
+use constant N => 5;
 my $SIZE = 1000;
 my $STEP = 1000;
+my @json = ();
 
 print "[\n";
 foreach my $p (10,12,14,16,18,20,25,30,35,40,50,60,80) {
   my $sum=0;
   my @timings = ();
   
+  print STDERR "$p: ";
   foreach my $i (1..N) {
     #\time --format="%E" ./main.out --step 1000 --height 1000 --width 1000 --thread 16
-    my $r=`\\ssh mic1 "TIMEFORMAT=%R && time ./main.mic.out $SIZE $STEP $p" 2>&1`;
+    my $r=`\\ssh mic1 "TIMEFORMAT=%R && time ./main.out $SIZE $STEP $p" 2>&1`;
     $sum=$sum + $r;
     $r =~ s/^\s+|\s+$//g;
     push @timings, $r;
     
-    #print $r." ";
+    print STDERR $r." ";
   }
-  my @sorted = sort @timings;
+  print STDERR "\n";
+  my @sorted = sort {$a <=> $b} @timings;
   my $avg = $sum/N;
-  print "[$p, $avg, " . join(", ", @sorted) . "],\n";
+  push @json, "[$p, $avg, " . join(", ", @sorted) . "]";
 }
-print "]\n";
+print join(",\n", @json) . "\n]\n";
--- test_xeon.pl
+++ test_xeon.pl
@@ -1,13 +1,15 @@
 #!/bin/perl -w
-use constant N => 15;
+use constant N => 5;
 my $SIZE = 2000;
 my $STEP = 1000;
+my @json = ();
 
 print "[\n";
-foreach my $p (6,8,10,12,14,16,18,20,25,30,35,40,50) {
+foreach my $p (1,2,4,6,8,10,12,14,16,18,20,25,30,40) {
   my $sum=0;
   my @timings = ();
   
+  print STDERR "$p: ";
   foreach my $i (1..N) {
     #\time --format="%E" ./main.out --step 1000 --height 1000 --width 1000 --thread 16
     my $r=`\\time --format="%e" ./main.out $SIZE $STEP $p 2>&1`;
@@ -15,10 +17,11 @@ foreach my $p (6,8,10,12,14,16,18,20,25,30,35,40,50) {
     $r =~ s/^\s+|\s+$//g;
     push @timings, $r;
     
-    #print $r." ";
+    print STDERR $r." ";
   }
-  my @sorted = sort @timings;
+  print STDERR "\n";
+  my @sorted = sort {$a <=> $b} @timings;
   my $avg = $sum/N;
-  print "[$p, $avg, " . join(", ", @sorted) . "],\n";
+  push @json, "[$p, $avg, " . join(", ", @sorted) . "]";
 }
-print "]\n";
+print join(",\n", @json) . "\n]\n";
