6310f4b898f9af2907dd20d93afd013e4d25b283
Federico Soave <fede.covr@gmail.com>
2016-02-18 17:53:54 +0100

    add FastFlow version + make targets

---
+++ main_ff.cpp
@@ -0,0 +1,33 @@
+#include "usage.hpp"
+#include "Matrix.hpp"
+
+#ifdef __MIC__
+	#define NO_DEFAULT_MAPPING
+#endif
+#include <ff/parallel_for.hpp>
+
+int main(int argc, char *argv[]) {
+	const int NPROCS = sysconf(_SC_NPROCESSORS_ONLN);
+
+	if (existArgument(argc, argv, "--help") || existArgument(argc, argv, "-h")) {
+		show_usage();
+		return 0;
+	}
+
+	gol_run run = parse_arguments(argc, argv, NPROCS);
+	Matrix m(run.height, run.width, true);
+
+	if (run.workers != 0) {
+		ff::ParallelFor pf;
+		for (long i = 0; i < run.steps; i++) {
+			pf.parallel_for_idx(0, run.height, 1, 0,
+				[&m](const long start, const long end, const int thid){
+					m.updateRows(start, end);
+				}, run.workers);
+			m.swap();
+		}
+	} else {
+		for (long i = 0; i < run.steps; i++) m.updateRows(0, run.height);
+	}
+	return 0;
+}
