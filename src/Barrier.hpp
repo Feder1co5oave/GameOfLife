92c1658c2689133587a9ed2f386409bacb7bfa68
Federico Soave <fede.covr@gmail.com>
2016-07-17 06:22:18 +0200

    inline the Barrier

--- src/Barrier.hpp
+++ src/Barrier.hpp
@@ -17,13 +17,32 @@ private:
 	barrier& operator= (barrier const&) = delete;
 
 public:
-	explicit barrier(int n);
+	explicit barrier(int n) :
+		N_THREADS(n),
+		counts {0, 0},
+		current(0),
+		lock(),
+		condition() { }
 
 	/*
 	 * Suspend the current thread until all thread have arrived at the barrier.
 	 * The thread that has come last executes cb() before awakening all other threads.
 	 * @returns true if it is the last thread, false otherwise
 	 */
-	bool await(function<void()> cb = []{});
-	virtual ~barrier() {}
+	bool await(function<void()> cb = []{}) {
+		unique_lock<mutex> _lock(lock);
+		int my = current;
+		counts[my]++;
+
+		if (counts[my] < N_THREADS) {
+			condition.wait(_lock, [&]{ return counts[my] == N_THREADS; });
+			return false;
+		} else {
+			current ^= 1;
+			counts[current] = 0;
+			if (cb) cb();
+			condition.notify_all();
+			return true;
+		}
+	}
 };
