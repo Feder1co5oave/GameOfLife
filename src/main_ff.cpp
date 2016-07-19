4853a8296f5b91eca114864258b5f213b6efeb50
Federico Soave <fede.covr@gmail.com>
2016-07-19 15:17:16 +0200

    use the spinwait variant of fastflow on the mic

--- src/main_ff.cpp
+++ src/main_ff.cpp
@@ -19,25 +19,25 @@ int main(int argc, char *argv[]) {
 	gol_run run = parse_arguments(argc, argv, NPROCS);
 	Matrix m(run.height, run.width, false);
 
-	if (run.workers != 0) {
-		ff::ParallelFor pf;
-		
+	#ifdef SPIN
+	ff::ParallelFor pf(run.workers, true, true);
+	#else
+	ff::ParallelFor pf(run.workers);
+	#endif
+	
+	pf.parallel_for_idx(0, run.height, 1, 0,
+		[&m](const long start, const long end, const int) {
+			m.randomizeRows(start, end);
+	}, run.workers);
+
+	if (run.check) cout << m << endl;
+
+	for (long i = 0; i < run.steps; i++) {
 		pf.parallel_for_idx(0, run.height, 1, 0,
-			[&m](const long start, const long end, const int) {
-				m.randomizeRows(start, end);
+			[&m](const long start, const long end, const int thid) {
+				m.updateRows(start, end);
 		}, run.workers);
-
-		if (run.check) cout << m << endl;
-
-		for (long i = 0; i < run.steps; i++) {
-			pf.parallel_for_idx(0, run.height, 1, 0,
-				[&m](const long start, const long end, const int thid) {
-					m.updateRows(start, end);
-			}, run.workers);
-			m.swap();
-		}
-	} else {
-		for (long i = 0; i < run.steps; i++) m.updateRows(0, run.height);
+		m.swap();
 	}
 
 	if (run.check) cout << m << endl;
